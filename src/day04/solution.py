d2e9d33d09859bb94fc6809985b45dac643a85d1
Vincent Kappert <vincent.kappert@gmail.com>
2024-12-08 17:41:23 +0100

    solved day4 part2

--- src/day04/solution.py
+++ src/day04/solution.py
@@ -15,8 +15,14 @@ def allLines(file_path):
         reverselines.append(line[::-1])
     result.extend(reverselines)
 
-
     return result
+
+def diagonalLines(file_path):
+    with open(file_path, "r") as file:
+        data = file.read().strip()
+    horlines = data.splitlines()
+
+    return getDiagonalsPart2(horlines)
 
 def getDiagonals(lines):
     # test = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
@@ -51,3 +57,45 @@ def part1(file_path):
     # print(len(lines))
     s = sum(line.count('XMAS') for line in lines)
     return s
+
+def part2(file_path):
+    lines = diagonalLines(file_path)
+    print(lines)
+    return len(lines)
+
+def getDiagonalsPart2(lines):
+    # test = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+    max_col = len(lines[0])
+    max_row = len(lines)
+    fdiag = [[] for _ in range(max_row + max_col - 1)]
+    bdiag = [[] for _ in range(len(fdiag))]
+    min_bdiag = -max_row + 1
+
+    for x in range(max_col):
+        for y in range(max_row):
+            fdiag[x+y].append((lines[y][x], (x,y)))
+            bdiag[x-y-min_bdiag].append((lines[y][x], (x,y)))
+
+    r = []
+    for fd in fdiag:
+        for i in range(1,len(fd)-1):
+            # print(fd[i])
+            # print(fd[i][0])
+            if fd[i][0] == 'A' and fd[i-1][0] == 'M' and fd[i+1][0] == 'S':
+                r.append(fd[i][1])
+            if fd[i][0] == 'A' and fd[i+1][0] == 'M' and fd[i-1][0] == 'S':
+                r.append(fd[i][1])
+
+    b = []
+    for bd in bdiag:
+        for i in range(1,len(bd)-1):
+            print(bd[i])
+            print(bd[i][0])
+            if bd[i][0] == 'A' and bd[i-1][0] == 'M' and bd[i+1][0] == 'S':
+                b.append(bd[i][1])
+            if bd[i][0] == 'A' and bd[i+1][0] == 'M' and bd[i-1][0] == 'S':
+                b.append(bd[i][1])
+
+    c = [item for item in b if item in r]
+    return c
--- tests/test_day04.py
+++ tests/test_day04.py
@@ -10,9 +10,15 @@ def sampleinput():
 def test_part1_numberOfXMAS(sampleinput):
     assert part1(sampleinput) == 18
 
+def test_part2_numberOfX_MAS(sampleinput):
+    assert part2(sampleinput) == 9
+
 @pytest.fixture
 def input():
     return os.path.join(os.path.dirname(__file__), "resources", "day04input.txt")
 
 def test_part1_numberOfXMASi(input):
     assert part1(input) == 2644
+
+def test_part2_numberOfX_MASi(input):
+    assert part2(input) == 1952
